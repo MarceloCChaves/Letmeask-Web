6d182c91409ff25921a2fe29558c7688f05a61fe
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-06-21 23:00:50 -0300

    Added: images in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Home from './pages/Home';
+import NewRoom from './pages/NewRoom';
+import { BrowserRouter, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
   );
 }
 
